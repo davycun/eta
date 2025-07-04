901bd3c595979c096a11b1bd9108ede72811ae41
davy <cundaiyong@metrodata.cn>
2025-07-04 10:04:56 +0800

    转发模块支持缓存

--- pkg/module/forward/forward_srv/controller.go
+++ pkg/module/forward/forward_srv/controller.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"github.com/davycun/eta/pkg/common/caller"
 	"github.com/davycun/eta/pkg/common/ctx"
+	"github.com/davycun/eta/pkg/common/errs"
 	"github.com/davycun/eta/pkg/core/controller"
 	"github.com/davycun/eta/pkg/module/forward"
-	"github.com/davycun/eta/pkg/module/setting"
 	"github.com/gin-gonic/gin"
 	"github.com/go-resty/resty/v2"
 	"io"
@@ -17,8 +17,16 @@ import (
 )
 
 func Forward(c *gin.Context) {
-	hd := newHandler(c)
-	err := hd.do(hd.newRequest()).Err()
+	var (
+		err error
+		hd  = newHandler(c)
+	)
+	if hd.err != nil {
+		controller.ProcessResult(c, nil, hd.err)
+		return
+	}
+	err = hd.do(hd.newRequest()).Err()
+
 	if err != nil {
 		controller.ProcessResult(c, nil, err)
 	}
@@ -26,34 +34,48 @@ func Forward(c *gin.Context) {
 }
 
 type handler struct {
-	c      *gin.Context
-	ct     *ctx.Context
-	vendor string
-	cred   setting.BaseCredentials
-	err    error
-	client *resty.Client
+	c          *gin.Context
+	ct         *ctx.Context
+	vendorName string
+	vendor     forward.Vendor
+	err        error
+	client     *resty.Client
+	cacheKey   string //请求缓存的hash key
+	realPath   string
+	needCache  bool
+}
+
+func (h *handler) Err() error {
+	return h.err
 }
 
 func newHandler(c *gin.Context) *handler {
 	hd := &handler{
 		c: c,
 	}
-	hd.vendor = c.Param("vendor")
+	hd.vendorName = c.Param(forward.PathVendor)
 	hd.ct = ctx.GetContext(c)
-	hd.cred, hd.err = forward.GetVendor(hd.ct.GetAppGorm(), hd.vendor)
+	hd.vendor, hd.err = forward.GetVendor(hd.ct.GetAppGorm(), hd.vendorName)
 
 	if hd.err != nil {
 		return hd
 	}
 
 	hd.client = resty.New()
-	if hd.cred.ProxyUrl != "" {
-		hd.client.SetProxy(hd.cred.ProxyUrl)
+	if hd.vendor.ProxyUrl != "" {
+		hd.client.SetProxy(hd.vendor.ProxyUrl)
 	}
-	hd.client.SetBaseURL(hd.cred.BaseUrl).
-		SetDebug(hd.cred.Debug).
-		SetTimeout(time.Second * time.Duration(hd.cred.Timeout)). //A Timeout of zero means no timeout.
-		SetTLSClientConfig(&tls.Config{InsecureSkipVerify: hd.cred.InsecureSkipVerify})
+	hd.client.SetBaseURL(hd.vendor.RandomBaseUrl()).
+		SetDebug(hd.vendor.Debug).
+		SetTimeout(time.Second * time.Duration(hd.vendor.Timeout)). //A Timeout of zero means no timeout.
+		SetTLSClientConfig(&tls.Config{InsecureSkipVerify: hd.vendor.InsecureSkipVerify})
+	if hd.vendor.Debug {
+		hd.client.EnableGenerateCurlOnDebug()
+	}
+
+	hd.realPath = c.Param(forward.PathParam)
+	hd.realPath = fmt.Sprintf("/%s", strings.TrimLeft(hd.realPath, "/")) //保证绝对路径斜杠开头，也避免前端路径传入多个斜杠
+	hd.needCache = hd.vendor.NeedCache(hd.c.Request.Method, hd.realPath)
 
 	return hd
 }
@@ -71,6 +93,7 @@ func (h *handler) newRequest() *resty.Request {
 			for k := range h.c.Request.Header {
 				req.SetHeader(k, h.c.GetHeader(k))
 			}
+			req.SetQueryParamsFromValues(h.c.Request.URL.Query())
 			return h.err
 		}).
 		Call(func(cl *caller.Caller) error {
@@ -83,8 +106,8 @@ func (h *handler) newRequest() *resty.Request {
 			return h.err
 		}).
 		Call(func(cl *caller.Caller) error {
-			if fc, ok := forward.GetHandleRequest(h.vendor); ok {
-				realBody, err1 := fc(req, h.cred, reqBody)
+			if fc, ok := forward.GetHandleRequest(h.vendorName); ok {
+				realBody, err1 := fc(req, h.vendor, reqBody)
 				if err1 != nil {
 					h.err = errors.New(fmt.Sprintf("request handler error %s", err1))
 				}
@@ -99,6 +122,12 @@ func (h *handler) newRequest() *resty.Request {
 				req.SetBody(reqBody)
 			}
 			return h.err
+		}).
+		Call(func(cl *caller.Caller) error {
+			if h.needCache {
+				h.cacheKey, h.err = forward.MakeCacheKey(h.c, reqBody, h.vendorName)
+			}
+			return h.err
 		}).Err
 
 	return req
@@ -106,66 +135,53 @@ func (h *handler) newRequest() *resty.Request {
 
 func (h *handler) do(req *resty.Request) *handler {
 
+	var (
+		resp      *resty.Response
+		cacheData = forward.CacheData{}
+	)
+	if h.needCache && h.err == nil {
+		cacheData, h.err = forward.LoadCacheData(h.cacheKey, h.vendor)
+		//说明cache有效
+		if cacheData.IsValid() {
+			h.c.Header("X-Cache-Key", h.cacheKey)
+			h.err = h.writeResponse(cacheData)
+			return h
+		}
+	}
 	if h.err != nil {
 		return h
 	}
 
-	var (
-		realPath = h.c.Param("path") //注意前端不要调用的时候传入多个斜杠
-		resp     *resty.Response
-	)
-	resp, h.err = req.Execute(h.c.Request.Method, realPath)
-
+	//调用实际请求
+	resp, h.err = req.Execute(h.c.Request.Method, h.realPath)
 	if resp == nil {
 		return h
 	}
-
-	//如果body为空可能是GET或者其他原因，如果之前发生了错误，就用delta的格式返回，如果没有就直接返回
-	var (
-		respBody = resp.Body()
-	)
-	writeHeader(h.c, resp)
-	if len(respBody) < 1 {
-		h.c.Abort()
+	cacheData, h.err = forward.MakeCacheData(h.vendor, resp)
+	if h.err != nil {
 		return h
 	}
-
-	if fc, ok := forward.GetHandleResponse(h.vendor); ok {
-		realBody, err1 := fc(resp, h.cred, respBody)
-		if err1 != nil {
-			_ = h.c.Error(err1)
-			//_ = h.c.Error(errors.New(fmt.Sprintf("response handler error %s", err1)))
-		}
-		if len(realBody) > 0 {
-			respBody = realBody
-		}
+	h.err = errs.Cover(h.err, h.writeResponse(cacheData))
+	if h.needCache {
+		h.err = errs.Cover(h.err, forward.SaveCacheData(h.cacheKey, h.vendor, cacheData))
 	}
+	return h
+}
 
-	if len(respBody) > 0 {
-		_, err := h.c.Writer.Write(respBody)
+func (h *handler) writeResponse(data forward.CacheData) error {
+	if h.err != nil {
+		return h.err
+	}
+	h.c.Writer.WriteHeader(data.Status)
+	for k := range data.Header {
+		h.c.Header(k, data.Header.Get(k))
+	}
+	if len(data.Body) > 0 {
+		_, err := h.c.Writer.Write(data.Body)
 		if err != nil {
 			_ = h.c.Error(err)
 		}
 	}
 	h.c.Abort()
-	return h
-}
-
-func (h *handler) Err() error {
 	return h.err
-}
-
-func writeHeader(c *gin.Context, resp *resty.Response) {
-	c.Writer.WriteHeader(resp.StatusCode())
-	for k := range resp.Header() {
-		//取消掉跨域相关的头信息和压缩相关的头信息
-		//resty和net/http都会处理gzip压缩，如果响应头中为Content-Encoding：gzip，说明服务端已经做了gzip压缩
-		//那么 net/http会做解压，可以定制http.Transport.DisableCompression为true来拒绝net/http解压
-		//但是如果net/http不解压了，resty还是会解压，但是resty不支持配置不解压
-		//所以从resty拿到的body是解压后的，直接往客户端写，但如果给客户端的响应头没有去除Content-Encoding: gzip，那么客户端会拿非压缩的数据进行gzip解压会报错
-		if strings.HasPrefix(k, "Cross") || strings.HasPrefix(k, "Content-Encoding") {
-			continue
-		}
-		c.Header(k, resp.Header().Get(k))
-	}
 }
