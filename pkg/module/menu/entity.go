03ed33058fbe1d6f6fc11054c181c8f290d3135c
davy <cundaiyong@metrodata.cn>
2025-07-28 14:33:23 +0800

    菜单新增Valid字段

--- pkg/module/menu/entity.go
+++ pkg/module/menu/entity.go
@@ -2,6 +2,7 @@ package menu
 
 import (
 	"github.com/davycun/eta/pkg/common/dorm"
+	"github.com/davycun/eta/pkg/common/dorm/ctype"
 	"github.com/davycun/eta/pkg/common/logger"
 	"github.com/davycun/eta/pkg/core/entity"
 	"github.com/davycun/eta/pkg/eta/constants"
@@ -23,16 +24,17 @@ type Type string
 
 type Menu struct {
 	entity.BaseEntity
-	Namespace string  `json:"namespace" gorm:"column:namespace;comment:命名空间;not null" binding:"required"`
-	ParentId  string  `json:"parent_id,omitempty" gorm:"column:parent_id;comment:菜单父ID"`
-	Type      Type    `json:"type,omitempty" gorm:"column:type;comment:菜单类型;not null"`
-	Api       ApiList `json:"api,omitempty" gorm:"column:api;serializer:json;comment:菜单访问地址"`
-	Name      string  `json:"name,omitempty" gorm:"column:name;comment:菜单名称;not null"`
-	Alias     string  `json:"alias,omitempty" gorm:"column:alias;comment:菜单别名"`
-	Icon      string  `json:"icon,omitempty" gorm:"column:icon;comment:菜单图标"`
-	Order     int     `json:"order,omitempty" gorm:"column:order;comment:菜单排序"`
-	Parent    *Menu   `json:"parent,omitempty" gorm:"-:all"`
-	Children  []*Menu `json:"children,omitempty" gorm:"-:all"`
+	Namespace string        `json:"namespace" gorm:"column:namespace;comment:命名空间;not null" binding:"required"`
+	ParentId  string        `json:"parent_id,omitempty" gorm:"column:parent_id;comment:菜单父ID"`
+	Type      Type          `json:"type,omitempty" gorm:"column:type;comment:菜单类型;not null"`
+	Api       ApiList       `json:"api,omitempty" gorm:"column:api;serializer:json;comment:菜单访问地址"`
+	Name      string        `json:"name,omitempty" gorm:"column:name;comment:菜单名称;not null"`
+	Alias     string        `json:"alias,omitempty" gorm:"column:alias;comment:菜单别名"`
+	Icon      string        `json:"icon,omitempty" gorm:"column:icon;comment:菜单图标"`
+	Order     int           `json:"order,omitempty" gorm:"column:order;comment:菜单排序"`
+	Valid     ctype.Boolean `json:"valid,omitempty"  gorm:"column:valid;comment:启用或禁用;default:'1'"`
+	Parent    *Menu         `json:"parent,omitempty" gorm:"-:all"`
+	Children  []*Menu       `json:"children,omitempty" gorm:"-:all"`
 }
 
 func (m Menu) TableName(namer schema.Namer) string {
