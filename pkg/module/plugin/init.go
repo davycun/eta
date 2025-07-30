b2ba88d5d0c4c4eee24392bb5459190d3f126151
davy <cundaiyong@metrodata.cn>
2025-07-30 11:31:40 +0800

    修复tree插件问题

--- pkg/module/plugin/init.go
+++ pkg/module/plugin/init.go
@@ -3,14 +3,12 @@ package plugin
 import (
 	"github.com/davycun/eta/pkg/core/iface"
 	"github.com/davycun/eta/pkg/core/service/hook"
-	"github.com/davycun/eta/pkg/module/app"
 	"github.com/davycun/eta/pkg/module/plugin/plugin_auth"
 	"github.com/davycun/eta/pkg/module/plugin/plugin_crypt"
 	"github.com/davycun/eta/pkg/module/plugin/plugin_es"
 	"github.com/davycun/eta/pkg/module/plugin/plugin_geo"
 	"github.com/davycun/eta/pkg/module/plugin/plugin_his"
 	"github.com/davycun/eta/pkg/module/plugin/plugin_push"
-	"github.com/davycun/eta/pkg/module/plugin/plugin_tree"
 )
 
 func InitPlugin() {
@@ -23,9 +21,6 @@ func InitPlugin() {
 	hook.AddRetrieveCallback(hook.CallbackForAll, plugin_crypt.VerifySign)
 	hook.AddRetrieveCallback(hook.CallbackForAll, plugin_crypt.StoreDecrypt)
 	hook.AddRetrieveCallback(hook.CallbackForAll, plugin_his.DeleteHistoryCallback)
-	hook.AddRetrieveCallback(hook.CallbackForAll, plugin_tree.TreeResult[app.App](), func(option *hook.CallbackOption) {
-		option.Order = 10000
-	})
 	hook.AddAuthCallback(hook.CallbackForAll, plugin_auth.AuthFilter, func(option *hook.CallbackOption) {
 		option.IsAuth = true
 		option.Methods = []iface.Method{iface.MethodAll}
