9ac6b5c759dd4c0777a3326c909fdb8edb7bd408
davy <cundaiyong@metrodata.cn>
2025-08-19 14:26:31 +0800

    新增工具类函数

--- pkg/common/utils/slice.go
+++ pkg/common/utils/slice.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"errors"
+	"github.com/duke-git/lancet/v2/slice"
+	"reflect"
+)
+
 func ConvertToSlice[T any](data any, dst *[]T) {
 
 	var (
@@ -28,4 +34,79 @@ func AppendNoEmpty(src []string, dst ...string) []string {
 		}
 	}
 	return src
+}
+
+func Chunk(data any, size int) ([]any, error) {
+	// data 切片
+	val := reflect.ValueOf(data)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Slice {
+		return nil, errors.New("data must be a slice type")
+	}
+	if val.Len() == 0 {
+		return nil, nil
+	}
+
+	var dataChunks []any
+	start, end := 0, size
+	for start < val.Len() {
+		if end > val.Len() {
+			end = val.Len()
+		}
+		dataChunks = append(dataChunks, val.Slice(start, end).Interface())
+		start = start + size
+		end = end + size
+	}
+	return dataChunks, nil
+}
+
+func SliceLen(data any) (int, error) {
+	// data 切片
+	val := reflect.ValueOf(data)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Slice {
+		return 0, errors.New("data must be a slice type")
+	}
+	return val.Len(), nil
+}
+
+func SliceRemoveElemByIndexes(data any, idx []int) (any, error) {
+	val := reflect.ValueOf(data)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Slice {
+		return nil, errors.New("data must be a slice type")
+	}
+
+	newData := reflect.MakeSlice(val.Type(), 0, val.Len())
+	if val.Len() == 0 {
+		return newData.Interface(), nil
+	}
+
+	for i := range val.Len() {
+		if slice.Contain(idx, i) {
+			continue
+		}
+		newData = reflect.Append(newData, val.Index(i))
+	}
+	return newData.Interface(), nil
+}
+
+func IsEmptySlice(data any) bool {
+	if data == nil {
+		return true
+	}
+	val := reflect.ValueOf(data)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	if !slice.Contain([]reflect.Kind{reflect.Slice, reflect.Array}, val.Kind()) {
+		return true
+	}
+	return val.Len() == 0
 }
