2afa143e132b250d6b9f6be22dff428ce5995360
davy <cundaiyong@metrodata.cn>
2025-08-20 18:25:13 +0800

    模板获取新增缓存机制

--- pkg/module/data/template/template_srv/service_callback.go
+++ pkg/module/data/template/template_srv/service_callback.go
@@ -2,6 +2,7 @@ package template_srv
 
 import (
 	"github.com/davycun/eta/pkg/common/caller"
+	"github.com/davycun/eta/pkg/core/iface"
 	"github.com/davycun/eta/pkg/core/service/hook"
 	"github.com/davycun/eta/pkg/module/data/template"
 )
@@ -37,6 +38,21 @@ func modifyCallback(cfg *hook.SrvConfig, pos hook.CallbackPosition) error {
 		}).
 		Call(func(cl *caller.Caller) error {
 			return hook.AfterUpdate(cfg, pos, afterUpdate)
+		}).
+		Call(func(cl *caller.Caller) error {
+			return hook.AfterModify(cfg, pos, func(cfg *hook.SrvConfig, oldValues []template.Template, newValues []template.Template) error {
+				template.DelCache(cfg.TxDB, oldValues...)
+				return nil
+			})
+		}).
+		Call(func(cl *caller.Caller) error {
+			return hook.AfterModify(cfg, pos, func(cfg *hook.SrvConfig, oldValues []template.Template, newValues []template.Template) error {
+				template.DelCache(cfg.TxDB, oldValues...)
+				if cfg.Method == iface.MethodCreate {
+					template.SetCacheHasAll(cfg.TxDB, false)
+				}
+				return nil
+			})
 		}).Err
 	return err
 }
