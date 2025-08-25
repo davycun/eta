691d42f91c46440feef82dfe0d7824e02224a775
davy <cundaiyong@metrodata.cn>
2025-08-25 11:52:45 +0800

    integration继承接口支持并发或顺序执行#48

--- pkg/module/integration/service.go
+++ pkg/module/integration/service.go
@@ -2,11 +2,16 @@ package integration
 
 import (
 	"github.com/davycun/eta/pkg/common/ctx"
-	"github.com/davycun/eta/pkg/common/dorm"
 	"github.com/davycun/eta/pkg/common/dorm/xa"
+	"github.com/davycun/eta/pkg/common/errs"
 	"github.com/davycun/eta/pkg/common/global"
+	"github.com/davycun/eta/pkg/common/logger"
+	"github.com/davycun/eta/pkg/common/run"
 	"github.com/davycun/eta/pkg/core/dto"
 	"github.com/davycun/eta/pkg/core/iface"
+	"github.com/davycun/eta/pkg/eta/ecf"
+	"gorm.io/gorm"
+	"sync"
 )
 
 type txService struct {
@@ -19,11 +24,12 @@ type txService struct {
 	Command    string
 }
 
-func TransactionCall(c *ctx.Context, srvList []txService, result *CommandResult) error {
+func transactionCall(c *ctx.Context, param *CommandParam, srvList []txService, result *CommandResult) error {
 	var (
 		err       error
-		appTxDb   = dorm.Transaction(c.GetAppGorm())
-		localTxDb = dorm.Transaction(global.GetLocalGorm())
+		appTxDb   = xa.Transaction(c.GetAppGorm())
+		localTxDb = xa.Transaction(global.GetLocalGorm())
+		wg        = sync.WaitGroup{}
 	)
 
 	defer func() {
@@ -32,41 +38,94 @@ func TransactionCall(c *ctx.Context, srvList []txService, result *CommandResult)
 		xa.CommitOrRollback(localTxDb, err)
 	}()
 
-	for _, txSrv := range srvList {
-		var (
-			srv iface.Service
-			//tb  = entity.GetContextTable(txSrv.C)
-			ec = iface.GetContextEntityConfig(txSrv.C)
-			rs = CommandResultItem{EntityCode: txSrv.EntityCode, Command: txSrv.Command, Result: txSrv.Result}
-		)
-		//如果某个表同时在localDB和appDB，那么暴露的服务只操作appDB的表
-		if ec.LocatedApp() {
-			srv = txSrv.NewSrv(txSrv.C, appTxDb, ec)
+	if !param.Order {
+		wg.Add(len(srvList))
+	}
+	for i := range srvList {
+		if param.Order {
+			err = errs.Cover(err, callTxServices(&srvList[i], localTxDb, appTxDb, result))
+			if err != nil {
+				return err
+			}
 		} else {
-			srv = txSrv.NewSrv(txSrv.C, localTxDb, ec)
+			run.Go(func() {
+				defer wg.Done()
+				if err != nil {
+					return
+				}
+				err = errs.Cover(err, callTxServices(&srvList[i], localTxDb, appTxDb, result))
+			})
 		}
-		//if tb.LocalDB {
-		//	srv = txSrv.NewSrv(txSrv.C, localTxDb, tb)
-		//} else {
-		//	srv = txSrv.NewSrv(txSrv.C, appTxDb, tb)
-		//}
+	}
+	wg.Wait()
+	return err
+}
 
-		switch iface.Method(txSrv.Command) {
-		case iface.MethodCreate:
-			err = srv.Create(txSrv.Param, txSrv.Result)
-		case iface.MethodUpdate:
-			err = srv.Update(txSrv.Param, txSrv.Result)
-		case iface.MethodUpdateByFilters:
-			err = srv.UpdateByFilters(txSrv.Param, txSrv.Result)
-		case iface.MethodDelete:
-			err = srv.Delete(txSrv.Param, txSrv.Result)
-		case iface.MethodDeleteByFilters:
-			err = srv.DeleteByFilters(txSrv.Param, txSrv.Result)
-		}
+func callTxServices(txSrv *txService, localTxDb, appTxDb *gorm.DB, result *CommandResult) error {
+	var (
+		//txSrv       = &srvList[i]
+		err         error
+		ec          = ecf.GetContextEntityConfig(txSrv.C)
+		srvListTemp = make([]iface.Service, 0, 1)
+	)
+	//如果某个表同时在localDB和appDB
+	if ec.LocatedLocal() {
+		cx := txSrv.C.Clone()
+		cx.SetContextGorm(localTxDb)
+		srvListTemp = append(srvListTemp, txSrv.NewSrv(cx, localTxDb, ec))
+	}
+	if ec.LocatedApp() {
+		cx := txSrv.C.Clone()
+		cx.SetContextGorm(appTxDb)
+		srvListTemp = append(srvListTemp, txSrv.NewSrv(cx, appTxDb, ec))
+	}
+
+	for _, srv := range srvListTemp {
+		err = callService(srv, txSrv, result)
 		if err != nil {
 			return err
 		}
-		result.Items = append(result.Items, rs)
 	}
 	return nil
+}
+
+func callService(srv iface.Service, txSrv *txService, result *CommandResult) error {
+
+	var (
+		err        error
+		args       = txSrv.Param
+		command    = txSrv.Command
+		rs         = txSrv.Result
+		entityCode = txSrv.EntityCode
+	)
+
+	switch iface.Method(command) {
+	case iface.MethodCreate:
+		err = srv.Create(args, rs)
+	case iface.MethodUpdate:
+		err = srv.Update(args, rs)
+	case iface.MethodUpdateByFilters:
+		err = srv.UpdateByFilters(args, rs)
+	case iface.MethodDelete:
+		err = srv.Delete(args, rs)
+	case iface.MethodDeleteByFilters:
+		err = srv.DeleteByFilters(args, rs)
+	case iface.MethodQuery:
+		err = srv.Query(args, rs)
+	case iface.MethodCount:
+		err = srv.Count(args, rs)
+	case iface.MethodDetail:
+		err = srv.Detail(args, rs)
+	case iface.MethodPartition:
+		err = srv.Partition(args, rs)
+	default:
+		logger.Errorf("the method[%s] is not support", command)
+		return err
+
+	}
+	if err == nil {
+		result.Items = append(result.Items, CommandResultItem{EntityCode: entityCode, Command: command, Result: rs})
+	}
+
+	return err
 }
